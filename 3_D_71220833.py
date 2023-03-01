618531c094c02560eb156cb136e54cf050cae484
Christ <117885718+ChristBernardS@users.noreply.github.com>
2023-03-01 18:05:07 +0700

    Add files via upload

--- 3_D_71220833.py
+++ 3_D_71220833.py
@@ -1,25 +1,14 @@
-# def nyari_panjang_kata(x):
-#     x.split(' ')
-#     i = 0
-#     d = 0
-#     while i < len(x):
-#         b = x[i]
-#         c = b.split()
-#         i += 1
-#         if c >= d:
-#             d = c
-#             e = i
-#     return x[e]
+def nyari_panjang_kata(x):
+    i = 0
+    d = 0
+    while i < len(x):
+        b = x[i]
+        c = list(b)
+        i += 1
+        if len(c) >= d:
+            d = len(c)
+            e = i
+    return x[e-1]
 
 a = input('Masukkan sebuah kalimat: ').split(' ')
-# print(f"Kata terpanjang dalam kalimat tersebut adalah: {nyari_panjang_kata(a)}")
-
-i = 0
-d = 0
-while i < len(a):
-    b = a[i]
-    c = b.split()
-    i += 1
-    if len(c) >= d:
-        d = len(c)
-        e = i
+print(f"Kata terpanjang dalam kalimat tersebut adalah: {nyari_panjang_kata(a)}")
